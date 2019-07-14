a319410afc19a7a01c0588f1d2cbed99cd943eb2
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-07-14 20:51:51 +0200

    правки

--- app/elements/header/header.js
+++ app/elements/header/header.js
@@ -3,13 +3,13 @@ $('.social-net').hover(function () {
 });
 
 $(document).scroll(() => {
-  console.log($(document).scrollTop())
+
   if ($(document).scrollTop() >= 50) {
     $('.header__lower').addClass('header__lower_active');
   } else if ($(document).scrollTop() < 50) {
     $('.header__lower').removeClass('header__lower_active');
   }
-  if ($(document).scrollTop() > 1600) {
+  if ($(document).scrollTop() > 1650) {
     $('.header__lower').removeClass('header__lower_active');
   }
 
@@ -55,10 +55,67 @@ doAnimation('audio-footer', 'swing', 2000, 5000);
 $(window).on('load', function () {
   var $preloader = $('#p_prldr'),
     $svg_anm = $preloader.find('.svg_anm');
-  $svg_anm.fadeOut();
+  //$svg_anm.fadeOut();
   $preloader.delay().fadeOut('slow');
 });
 
 $('.burger').click(() => {
   $('.header .nav').toggleClass('nav_active');
 })
+
+
+
+if ($(window).width() < 768) {
+  $('.burger').click(function () {
+
+    $(this).toggleClass('burger_active');
+  })
+}
+
+
+
+//проверка был ли скролл вверх или вних и подсвечиваем пункты в меню
+var lastScrollTop = 0;
+
+$(window).scroll(function () {
+
+  var st = $(this).scrollTop();
+  if (st > lastScrollTop) { //если прокрутка вних
+
+    var $sections = $('.section'); //секции в которых якорные ссылки
+
+    $sections.each(function (i, el) {
+      var top = $(el).offset().top - 120;
+      var bottom = top + $(el).height();
+      var scroll = $(window).scrollTop();
+      var id = $(el).attr('id');
+      if (scroll > top && scroll < bottom) {
+        $('.header a').removeClass('nav__link_active');
+        $(' .nav_header a[href="#' + id + '"]').addClass('nav__link_active');
+      } else if (st < 700) {
+        $('.nav_header a:first').addClass('nav__link_active');
+      }
+    })
+
+  } else { // если прокрутка вверх
+
+    var $sections = $('.section'); //секции в которых якорные ссылки
+
+    $sections.each(function (i, el) {
+      var top = $(el).offset().top - 120;
+      var bottom = top + $(el).height();
+      var scroll = $(window).scrollTop();
+      var id = $(el).attr('id');
+      if (scroll > top && scroll < bottom) {
+        $('.header a').removeClass('nav__link_active');
+        $(' .nav_header a[href="#' + id + '"]').addClass('nav__link_active');
+      } else if (st < 700) {
+        $('.nav_header a:first').addClass('nav__link_active');
+      }
+    })
+
+  }
+
+  lastScrollTop = st;
+
+});
--- build/js/custom.js
+++ build/js/custom.js
@@ -76,13 +76,13 @@ $('document').ready(function () {
   });
 
   $(document).scroll(function () {
-    console.log($(document).scrollTop());
+
     if ($(document).scrollTop() >= 50) {
       $('.header__lower').addClass('header__lower_active');
     } else if ($(document).scrollTop() < 50) {
       $('.header__lower').removeClass('header__lower_active');
     }
-    if ($(document).scrollTop() > 1600) {
+    if ($(document).scrollTop() > 1650) {
       $('.header__lower').removeClass('header__lower_active');
     }
 
@@ -126,12 +126,64 @@ $('document').ready(function () {
   $(window).on('load', function () {
     var $preloader = $('#p_prldr'),
         $svg_anm = $preloader.find('.svg_anm');
-    $svg_anm.fadeOut();
+    //$svg_anm.fadeOut();
     $preloader.delay().fadeOut('slow');
   });
 
   $('.burger').click(function () {
     $('.header .nav').toggleClass('nav_active');
+  });
+
+  if ($(window).width() < 768) {
+    $('.burger').click(function () {
+
+      $(this).toggleClass('burger_active');
+    });
+  }
+
+  //проверка был ли скролл вверх или вних и подсвечиваем пункты в меню
+  var lastScrollTop = 0;
+
+  $(window).scroll(function () {
+
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      //если прокрутка вних
+
+      var $sections = $('.section'); //секции в которых якорные ссылки
+
+      $sections.each(function (i, el) {
+        var top = $(el).offset().top - 120;
+        var bottom = top + $(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+        if (scroll > top && scroll < bottom) {
+          $('.header a').removeClass('nav__link_active');
+          $(' .nav_header a[href="#' + id + '"]').addClass('nav__link_active');
+        } else if (st < 700) {
+          $('.nav_header a:first').addClass('nav__link_active');
+        }
+      });
+    } else {
+      // если прокрутка вверх
+
+      var $sections = $('.section'); //секции в которых якорные ссылки
+
+      $sections.each(function (i, el) {
+        var top = $(el).offset().top - 120;
+        var bottom = top + $(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+        if (scroll > top && scroll < bottom) {
+          $('.header a').removeClass('nav__link_active');
+          $(' .nav_header a[href="#' + id + '"]').addClass('nav__link_active');
+        } else if (st < 700) {
+          $('.nav_header a:first').addClass('nav__link_active');
+        }
+      });
+    }
+
+    lastScrollTop = st;
   });
   /* var modalAboutMe = new Kmodal('#modal-about-me'); */
   var num = 15;
